5df6631427b14d1deceba3157324508e00d8d074
susanynz <susanynz>
2024-02-18 21:49:50 -0600

    notas

--- fibonacci.js
+++ fibonacci.js
@@ -4,8 +4,10 @@ function calcularFibonacci() {
   //resultado a arrojar en pantalla 
   let resultadoDiv = document.getElementById("resultado");
 
+  //toma el numero dado y lo transforma en un integral
   let numero = parseInt(numeroInput);
 
+  //validar los numeros dados
   if (!validarEntrada(numero)) {
       resultadoDiv.innerHTML = "Por favor, ingrese un número entero positivo válido.";
       return;
@@ -20,8 +22,11 @@ function validarEntrada(dato) {
 }
 
 function fibonacciSerie(n) {
+  //inicia la serie Fibonacci con los dos primeros números
   let serie = [0, 1];
-
+//el bucle for comienza desde  desde i = 2 (ya que ya se tienen 0 y 1) hasta i < n. 
+//En cada iteración, se calcula el siguiente número de Fibonacci sumando los dos números anteriores y se agrega a la serie. 
+//Al final, la función devuelve la serie completa.
   for (let i = 2; i < n; i++) {
       serie[i] = serie[i - 1] + serie[i - 2];
   }
